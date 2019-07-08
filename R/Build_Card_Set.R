f4c2f2206d6e27a97857ee0c4003bcc7470eef57
Craig Jones <jones.dcraig@gmail.com>
2019-07-07 23:38:50 -0400

    Add randomization to faro "solution" matrix

--- R/Build_Card_Set.R
+++ R/Build_Card_Set.R
@@ -3,14 +3,8 @@
 #       Addition Only
 #       1 - 10
 # 
-# Solutions:
-#   4	=	2+2	1+3	3+1						
-#   5	=	2+3	3+2	1+4	4+1					
-#   6	=	1+5	5+1	2+4	4+2	3+3				
-#   7	=	1+6	6+1	2+5	5+2	3+4	4+3			
-#   8	=	1+7	7+1	2+6	6+2	5+3	3+5	4+4		
-#   9	=	1+8	8+1	2+7	7+2	6+3	3+6	4+5	5+4	
-#   10	=	1+9	9+1	2+8	8+2	3+7	7+3	4+6	6+4	5+5
+
+load("./data/dobble.RData")
 
 ### Problem Matrix
 problems <- matrix(c(
@@ -26,13 +20,15 @@ problems <- matrix(c(
 ### Solution Matrix
 solutions <- dobble[[1]]
 
-### Position Matrix
-position <- matrix(rep("...", length(problems)), ncol=3)
+# randomize solutions
+set.seed(as.numeric(Sys.Date()))
+solutions <- solutions[,sample(1:ncol(solutions))]
 
-for (i in 0:6) {
+### Position Matrix
+position <- matrix(rep("...", length(solutions)), ncol=ncol(solutions))
+
+for (i in 0:(nrow(solutions)-1)) {
   . <- which(solutions==i)
-  for (j in 1:3)
+  for (j in 1:ncol(solutions))
     position[.[j]] <- problems[i+1,j]
 } # end i
-
-
